4aa292e2e3ca9ef30b855a6862cb667b18650688
jaswanth <jaswanthchenni48@gmail.com>
2026-01-28 21:10:23 +0530

    Add PingController for /ping endpoint

---
+++ MedicineBackendApplication.java
@@ -0,0 +1,8 @@
+package com.medicinedetection;
+
+@SpringBootApplication
+public class MedicineBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedicineBackendApplication.class, args);
+    }
+}
